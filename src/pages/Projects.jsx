395a86705478e9c86129b92af29b0737641fb1da
Richard Feng <richardfeng101@gmail.com>
2026-06-14 17:53:09 -0700

    added 'present' as a possible date range

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,6 +8,11 @@ import "../styles/Projects.css";
 
 function parseDate(str) {
   const endPart = str.split("-").pop().trim();
+
+  if (endPart === "Present") {
+    return new Date(9999, 11, 31);
+  }
+
   return new Date(`${endPart} 1`);
 }
 
